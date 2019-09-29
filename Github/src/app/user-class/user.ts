f620659a176c53afe1ed91abbb908db903d49599
Nkurumwa <nkurumwa51@gmail.com>
2019-09-29 14:27:54 +0300

    comment out some code

--- Github/src/app/user-class/user.ts
+++ Github/src/app/user-class/user.ts
@@ -1,2 +1,10 @@
 export class User {
+  avatar: any;  username: string;
+  name: string;
+  bio: any;
+  repositories: number;
+  followers: number;
+  following: number;
+  link: any;
+
 }
--- Github/src/app/user-http/user.service.ts
+++ Github/src/app/user-http/user.service.ts
@@ -1,17 +1,71 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { User } from '../user-class/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  userName: string;
-  userRequest() {
-    throw new Error("Method not implemented.");
-  }
-  user: import("/home/moringa/Desktop/moringa-school-projects/Githubsearch/Github/src/app/user-class/user").User;
-  userLookup(userName: string) {
-    throw new Error("Method not implemented.");
-  }
 
-  constructor() { }
+  user: User;
+  userName: string;
+
+  constructor(private http:HttpClient){
+    this.user = new User("","","","","","","", "", new Date());
+  }
+  
+
+    userRequest(){
+
+      interface ApiResponse{
+        login: string;
+        name: string;
+        bio: any;
+        public_repos: number;
+        followers: number;
+        following: number;
+        avatar_url: any;
+        html_url: any;
+      }
+      let promise = new Promise((resolve,reject)=>{
+        this.http.get<ApiResponse>(environment.apiUrl + this.userName + "?access_token=" + environment.access_token).toPromise().then(response=>{
+
+          this.user.avatar = response.avatar_url
+          this.user.username = response.login
+          // this.user.name = response.name
+          // this.user.bio = response.bio
+          this.user.repositories = response.public_repos
+          // this.user.followers = response.followers
+          // this.user.following = response.following
+          this.user.link = response.html_url
+          
+
+        resolve();
+
+      },
+      error=>{
+
+          this.user.avatar = "https://avatars1.githubusercontent.com/u/47349274?v=4"
+          this.user.username = "Nkurumwa"
+          // this.user.name = "Hussein Fuaad"
+          // this.user.bio = "Full-Stack Developer \r\nAndroid Developer\r\nCertified Chef\r\nRelationship Officer"
+          // this.user.repositories = 22
+          // this.user.link = "https://github.com/fuaad001"
+
+        reject(error);
+
+
+        })
+      })
+
+      return promise
+
+    }
+
+    userLookup(userName: string){
+      this.userName = userName;
+    }
+    
+
 }
