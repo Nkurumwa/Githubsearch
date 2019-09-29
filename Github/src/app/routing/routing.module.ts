4d30511779371cb19c49fb6d6c684aef311fa66d
Nkurumwa <nkurumwa51@gmail.com>
2019-09-29 14:13:40 +0300

    adds content to routing module

--- Github/src/app/routing/routing.module.ts
+++ Github/src/app/routing/routing.module.ts
@@ -1,14 +1,35 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+
+// import { RoutingRoutingModule } from './routing-routing.module';
+
+
+// @NgModule({
+//   declarations: [],
+//   imports: [
+//     CommonModule,
+//     RoutingRoutingModule
+//   ]
+// })
+// export class RoutingModule { }
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
 
-import { RoutingRoutingModule } from './routing-routing.module';
+import { ShowComponent } from '../show/show.component'
+import { RepositoryComponent } from '../repository/repository.component'
 
+const routes: Routes = [
+  {path:"show",component:ShowComponent},
+  {path:"repo",component:RepositoryComponent}
+]
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RoutingRoutingModule
-  ]
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule],
 })
 export class RoutingModule { }
