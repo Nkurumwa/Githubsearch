1e18ac0e60367a6f532f13b97aea8243007a3d6b
Nkurumwa <nkurumwa51@gmail.com>
2019-09-29 13:17:02 +0300

    generates a new component

--- Github/src/app/app.module.ts
+++ Github/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoryComponent
   ],
   imports: [
     BrowserModule,
--- Github/src/app/repo.ts
+++ Github/src/app/repo.ts
@@ -1,2 +1,3 @@
 export class Repo {
+    constructor(public reposArray: {}){}
 }
