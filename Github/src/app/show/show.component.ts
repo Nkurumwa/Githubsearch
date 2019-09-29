000ee2f839f2aa71525a312829f920f9df663ae4
Nkurumwa <nkurumwa51@gmail.com>
2019-09-29 14:22:27 +0300

    modifies show component

--- Github/src/app/show/show.component.ts
+++ Github/src/app/show/show.component.ts
@@ -1,15 +1,51 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-show',
+//   templateUrl: './show.component.html',
+//   styleUrls: ['./show.component.css']
+// })
+// export class ShowComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
 import { Component, OnInit } from '@angular/core';
+import { RepoService } from '../repo-http/repo.service';
+import { User } from '../user-class/user';
+import { UserService } from '../user-http/user.service';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Component({
-  selector: 'app-show',
-  templateUrl: './show.component.html',
-  styleUrls: ['./show.component.css']
+  selector: 'app-display',
+  templateUrl: './display.component.html',
+  providers: [RepoService, UserService],
+  styleUrls: ['./display.component.css']
 })
-export class ShowComponent implements OnInit {
+export class DisplayComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  userName: string;
 
-  ngOnInit() {
+  constructor( private userRequestService: UserService){
+    this.userRequestService.userName = "Nkurumwa";
+    this.userRequestService.userRequest();
+    this.user = this.userRequestService.user;
+  }
+  
+
+  userLookup(){
+    this.userRequestService.userLookup(this.userName);
+    this.userRequestService.userRequest();
+    this.user = this.userRequestService.user; 
   }
 
+  ngOnInit() { 
+    
+   }
 }
--- Github/src/app/user-http/user.service.ts
+++ Github/src/app/user-http/user.service.ts
@@ -4,6 +4,14 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UserService {
+  userName: string;
+  userRequest() {
+    throw new Error("Method not implemented.");
+  }
+  user: import("/home/moringa/Desktop/moringa-school-projects/Githubsearch/Github/src/app/user-class/user").User;
+  userLookup(userName: string) {
+    throw new Error("Method not implemented.");
+  }
 
   constructor() { }
 }
